e76bd8ec71a8c7cf539c5be831641c96faa77d7c
Artur Kaluza <akaluza26@gmail.com>
2017-11-30 15:03:08 +0100

    Fixed undefind

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 function drawTree(n) {
     for (let i =0; i < n; i++) {
+        
         let star = "";
+        let space = ((n-i)/2).toFixed();
+        
+        for (let k = 0; k < space; k++) {
+            star += ' ';
+        }
+        
         for (let j = 0; j < i; j++) {
             star += '*';
         }
         console.log(star);
     }
-    return '';
 }
 
-console.log(drawTree(6));
-console.log(drawTree(4));
-console.log(drawTree(8));
+drawTree(6);
+drawTree(4);
+drawTree(8);
+
